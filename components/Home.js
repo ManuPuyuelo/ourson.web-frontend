9586c8e0cc211e16ce6e6c50d5264bc4e149c499
Manu Puyuelo <puyuelo.m@gmail.com>
2023-05-27 22:25:33 +0200

    test

--- components/Home.js
+++ components/Home.js
@@ -2,11 +2,11 @@ import styles from "../styles/Home.module.css";
 
 function Home() {
   return (
-    <div>
+    <>
       <main className={styles.main}>
         <h1 className={styles.title}>Bienvenue sur Ourson</h1>
       </main>
-    </div>
+    </>
   );
 }
 
