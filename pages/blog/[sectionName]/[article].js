533343634e97ffda77452ed53e51e8c6b7ee9cb7
Manu Puyuelo <puyuelo.m@gmail.com>
2023-06-17 18:41:29 +0200

    addition of blog dynamic path articleData component and sections

--- pages/blog/[sectionName]/[article].js
+++ pages/blog/[sectionName]/[article].js
@@ -1,5 +1,5 @@
-import Header from "../components/Header";
-import Article from "../components/Article";
+import Header from "../../../components/Header";
+import Article from "../../../components/Article";
 
 function ArticlePage() {
   return (
