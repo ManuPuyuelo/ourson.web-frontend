74f492f1b96725d14ee0e63ec169c5c4ffe8d30c
Manu Puyuelo <puyuelo.m@gmail.com>
2023-06-19 11:53:53 +0200

    generation static paths for articles then preview of it before styling

--- components/Article.js
+++ components/Article.js
@@ -3,13 +3,18 @@ import styles from "../styles/Article.module.css";
 import { useRouter } from "next/router";
 
 export default function Article({ article }) {
-  const bodyContent = article.content.body.map(
-    (data, i) => `<p key=${i}>${data.content}</p>`
-  );
+  const bodyContent = article.content.body.map((data, i) => (
+    <p key={i}>{data.content}</p>
+  ));
 
   return (
     <article>
-      <h1>{article.content.title}</h1>
+      <br /> <br /> <br /> <br /> <br />
+      <h1>
+        🚧 Cette page est en travaux et devrait être disponible dès ce soir
+        (19/06/2023)
+      </h1>
+      <h3>{article.content.title}</h3>
       <img src={article.imageURL} alt={article.content.title} />
       <p>{article.content.summary}</p>
       <p>{article.content.longSummary}</p>
